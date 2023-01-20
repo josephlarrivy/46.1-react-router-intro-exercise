aa63ee3aaa3cfe988257f0ca1d0608b1b421530a
josephlarrivy <joseph.larrivy@gmail.com>
2023-01-20 10:06:14 -0600

    Question sent to TA

--- src/Nav.js
+++ src/Nav.js
@@ -4,11 +4,21 @@ import { NavLink } from "react-router-dom";
 function NavBar() {
     return (
         <nav className="NavBar">
+
             <NavLink exact to="/">
                 VendingMachine
             </NavLink>
+
             <NavLink exact to="/chips">
                 chips
+            </NavLink>
+
+            <NavLink exact to="/candy">
+                candy
+            </NavLink>
+
+            <NavLink exact to="/soda">
+                soda
             </NavLink>
         </nav>
     );
